ed826c5c2e1f28044dc6b210b5cf027739598b05
simo <simone.riolfo07@gmail.com>
2025-02-05 20:42:02 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function mostraMessaggio() {
+    alert("Ti amo dalla luna al ritorno, ma per due volte <3");
+}
+
+function mostraMessaggio2() {
+    alert("Enri se hai cliccato questo bottone significa che sei in un momento giù :(. Ma non preoccuparti, per tua fortuna (spero ahah) hai un super fidanzato (però questo lo devi decidere tu ahah) che è disponibile ad ascoltarti! Quindi sei vuoi parlargli lui è molto felice di aiutarti ;)");
+}
